257b24c5197f0e7e531c0fa285273c49f048e122
ShadrinaM <143996514+ShadrinaM@users.noreply.github.com>
2025-10-21 18:33:22 +0300

    работает но с постгресом

--- CompiTeh-Repair_log/obj/Debug/net8.0-windows/CompiTeh-Repair_log.AssemblyInfo.cs
+++ CompiTeh-Repair_log/obj/Debug/net8.0-windows/CompiTeh-Repair_log.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompiTeh-Repair_log")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6ede943fa2ac58c2ea2a5834a0b53f042a62caa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97372d1158a729a2d74bc29c3d56f77e097b75c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompiTeh-Repair_log")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompiTeh-Repair_log")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
